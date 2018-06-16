c6e6be81f80a6b4a5c24fd034eb7dff6c9924601
carolito08 <39628602+carolito08@users.noreply.github.com>
2018-06-15 19:59:27 -0500

    Update TestMyController.java

--- vertx-sample/src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
+++ vertx-sample/src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
@@ -10,10 +10,10 @@ import static mx.unam.dgpe.sample.controller.RestUtil.*;
 public class TestMyController extends AbstractVerticle {
     private static final Logger logger = Logger.getLogger(TestMyController.class);
     
-   // @Test
+    @Test
     public void ok() throws Exception {
 	//String result= sendGet("http://192.168.62.128:8080/api/tercero?mode=sedsed");
-	String result= sendGet("http://localhost:8080/api/calcu?tipo=suma&num1=15&num2=5");
+	String result= sendGet("http://localhost:8080/api/calcutipo=suma&num1=15&num2=5");
         logger.info(result);
 
 	result= sendGet("http://localhost:8080/api/calcu?tipo=resta&num1=15&num2=5");
